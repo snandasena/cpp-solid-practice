946ef5bb8c8ad93b8115151bb18c1571c56e7a21
snandasena <sajithgithub@gmail.com>
2021-12-02 19:57:50 +0530

    Init

---
+++ tests/ut/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by sajith on 12/2/21.
+//
+
+#include <gtest/gtest.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
